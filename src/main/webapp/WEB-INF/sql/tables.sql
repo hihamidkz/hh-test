dd020c34ebfdfa760f95f0fcd03270ac358bae50
Hamidulla Kazimov <hihamidkz@inbox.ru>
2019-07-28 13:34:11 +0700

    Added model hints to the service builder

--- src/main/webapp/WEB-INF/sql/tables.sql
+++ src/main/webapp/WEB-INF/sql/tables.sql
@@ -1,8 +1,8 @@
 create table hh_api_Vacancy (
 	id_ LONG not null primary key,
 	name VARCHAR(300) null,
-	publishedAt VARCHAR(75) null,
+	publishedAt VARCHAR(300) null,
 	employer VARCHAR(300) null,
-	salary VARCHAR(75) null,
+	salary VARCHAR(300) null,
 	address VARCHAR(300) null
 );
